f74269277655a77d12e1a99c684e4c3737da8ff7
Fabio Gaiera <fagaiera@users.noreply.github.com>
2017-10-01 22:04:59 +0300

    AfterThrowing

---
+++ 02 Aspect Oriented Programming/063 AfterThrowing/src/main/java/io/berkel/afterthrowing/annotation/aspect/UserAspect.java
@@ -0,0 +1,18 @@
+package io.berkel.afterthrowing.annotation.aspect;
+
+import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+
+@Aspect
+public class UserAspect {
+
+    @Pointcut("execution(* io.berkel.afterthrowing.annotation.service.*.*(..))")
+    private void pointcut(){}
+
+    @AfterThrowing(pointcut = "pointcut()", throwing = "exception")
+    public void adviceForExceptionThrowing(Exception exception) throws Throwable {
+        System.out.println("###### " + exception.getMessage() + " ######");
+    }
+
+}
