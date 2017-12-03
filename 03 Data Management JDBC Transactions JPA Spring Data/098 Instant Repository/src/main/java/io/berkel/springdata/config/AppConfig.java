b199a864fa57f1414844917dd6e2c4ff90352f7f
Fabio Gaiera <fagaiera@users.noreply.github.com>
2017-12-04 01:29:28 +0200

    Instant repository

--- 03 Data Management JDBC Transactions JPA Spring Data/098 Instant Repository/src/main/java/io/berkel/springdata/config/AppConfig.java
+++ 03 Data Management JDBC Transactions JPA Spring Data/098 Instant Repository/src/main/java/io/berkel/springdata/config/AppConfig.java
@@ -9,67 +9,64 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
 import io.berkel.springdata.service.UserService;
 
 @Configuration
-@EnableJpaRepositories(basePackages = {
-        "io.berkel.springdata.model"
-})
+@EnableJpaRepositories(basePackages = { "io.berkel.springdata.model" })
 @EnableTransactionManagement
 public class AppConfig {
 
-    @Bean
-    public DataSource dataSource() {
+	@Bean(destroyMethod = "close")
+	DataSource dataSource(Environment env) {
+		
+		HikariConfig dataSourceConfig = new HikariConfig();
+		dataSourceConfig.setDriverClassName(env.getRequiredProperty("db.driver"));
+		dataSourceConfig.setJdbcUrl(env.getRequiredProperty("db.url"));
+		dataSourceConfig.setUsername(env.getRequiredProperty("db.username"));
+		dataSourceConfig.setPassword(env.getRequiredProperty("db.password"));
+		return new HikariDataSource(dataSourceConfig);
+		
+	}
 
-        DriverManagerDataSource ds = new DriverManagerDataSource();
-        ds.setDriverClassName("com.mysql.jdbc.Driver");
-        ds.setUrl("jdbc:mysql://localhost:3306/world?useSSL=false");
-        ds.setUsername("root");
-        ds.setPassword("root");
-        return ds;
+	@Bean
+	public UserService userService() {
+		return new UserService();
+	}
 
-    }
+	@Bean
+	JpaTransactionManager transactionManager(EntityManagerFactory entityManagerFactory) {
 
-    @Bean
-    public UserService userService() {
-        return new UserService();
-    }
-    
-    @Bean
-    JpaTransactionManager transactionManager(EntityManagerFactory entityManagerFactory) {
-        
-    	JpaTransactionManager transactionManager = new JpaTransactionManager();
-        transactionManager.setEntityManagerFactory(entityManagerFactory);
-        return transactionManager;
-        
-    }
-    
-    @Bean
-    LocalContainerEntityManagerFactoryBean entityManagerFactory(DataSource dataSource, Environment env) {
-        
-    	LocalContainerEntityManagerFactoryBean entityManagerFactoryBean = new LocalContainerEntityManagerFactoryBean();
-        entityManagerFactoryBean.setDataSource(dataSource);
-        entityManagerFactoryBean.setJpaVendorAdapter(new HibernateJpaVendorAdapter());
-        entityManagerFactoryBean.setPackagesToScan("net.petrikainulainen.springdata.jpa.todo");
- 
-        Properties jpaProperties = new Properties();
-        jpaProperties.put("hibernate.dialect", env.getRequiredProperty("hibernate.dialect"));
-        jpaProperties.put("hibernate.hbm2ddl.auto", env.getRequiredProperty("hibernate.hbm2ddl.auto"));
-        jpaProperties.put("hibernate.ejb.naming_strategy", env.getRequiredProperty("hibernate.ejb.naming_strategy"));
-        jpaProperties.put("hibernate.show_sql", env.getRequiredProperty("hibernate.show_sql"));
-        jpaProperties.put("hibernate.format_sql", env.getRequiredProperty("hibernate.format_sql"));
-        entityManagerFactoryBean.setJpaProperties(jpaProperties);
- 
-        return entityManagerFactoryBean;
-        
-    }
-    
-    	
+		JpaTransactionManager transactionManager = new JpaTransactionManager();
+		transactionManager.setEntityManagerFactory(entityManagerFactory);
+		return transactionManager;
+
+	}
+
+	@Bean
+	LocalContainerEntityManagerFactoryBean entityManagerFactory(DataSource dataSource, Environment env) {
+
+		LocalContainerEntityManagerFactoryBean entityManagerFactoryBean = new LocalContainerEntityManagerFactoryBean();
+		entityManagerFactoryBean.setDataSource(dataSource);
+		entityManagerFactoryBean.setJpaVendorAdapter(new HibernateJpaVendorAdapter());
+		entityManagerFactoryBean.setPackagesToScan("io.berkel.springdata.model");
+		Properties jpaProperties = new Properties();
+		jpaProperties.put("hibernate.dialect", env.getRequiredProperty("hibernate.dialect"));
+		jpaProperties.put("hibernate.hbm2ddl.auto", env.getRequiredProperty("hibernate.hbm2ddl.auto"));
+		jpaProperties.put("hibernate.ejb.naming_strategy", env.getRequiredProperty("hibernate.ejb.naming_strategy"));
+		jpaProperties.put("hibernate.show_sql", env.getRequiredProperty("hibernate.show_sql"));
+		jpaProperties.put("hibernate.format_sql", env.getRequiredProperty("hibernate.format_sql"));
+		entityManagerFactoryBean.setJpaProperties(jpaProperties);
+
+		return entityManagerFactoryBean;
+
+	}
 
 }
