6b61ac3321237e8eba2ba7ab364a43dce941e774
Fabio Gaiera <fagaiera@users.noreply.github.com>
2018-01-30 01:57:21 +0200

    RequestBody & ResponseBody

---
+++ 06 REST/147 ResponseBody/src/main/java/io/berkel/messageconverter/WebConfiguration.java
@@ -0,0 +1,21 @@
+package io.berkel.messageconverter;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import java.util.List;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan
+public class WebConfiguration extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> httpMessageConverters) {
+        httpMessageConverters.add(new BookCaseMessageConverter(new MediaType("text", "csv")));
+    }
+}
