bd25bfaf13d5392159f837df22632ac2ea179c31
Fabio Gaiera <fagaiera@users.noreply.github.com>
2018-03-03 18:45:04 +0200

    JoinPoint

--- 02 Aspect Oriented Programming/060 JoinPoint argument/src/main/java/io/berkel/joinpoint/annotation/aspect/UserAspect.java
+++ 02 Aspect Oriented Programming/060 JoinPoint argument/src/main/java/io/berkel/joinpoint/annotation/aspect/UserAspect.java
@@ -1,10 +1,8 @@
 package io.berkel.joinpoint.annotation.aspect;
 
 import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
-import org.aspectj.lang.annotation.Pointcut;
 import org.aspectj.lang.reflect.MethodSignature;
 
 import java.util.Arrays;
@@ -16,7 +14,6 @@ public class UserAspect {
     public void userBeforeAdvice(JoinPoint jp){
         MethodSignature signature = (MethodSignature) jp.getSignature();
         System.out.println("From the @Before of the aspect, advised method parameters: " + Arrays.asList(signature.getParameterNames()));
-
     }
 
 }
