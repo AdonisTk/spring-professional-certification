1caa4d6dae00bd505f342243a2e895ba11f7d2cb
Fabio Gaiera <fagaiera@users.noreply.github.com>
2018-03-07 13:10:11 +0200

    @RequestParam

--- 04 Spring MVC and the Web Layer/109 RequestParam/src/main/java/io/berkel/requestparam/controller/ArticleController.java
+++ 04 Spring MVC and the Web Layer/109 RequestParam/src/main/java/io/berkel/requestparam/controller/ArticleController.java
@@ -3,20 +3,17 @@ package io.berkel.requestparam.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
- * 
  * http://localhost:8080/requestparam/articles/get?id=20
- *
  */
 @Controller
 @RequestMapping("/articles")
 public class ArticleController {
-	
-	@RequestMapping("/get")
-	public String get(@RequestParam("id") int id) {
-		return "get";
-	}
-	
+
+    @RequestMapping("/get")
+    public String get(@RequestParam("id") int id) {
+        return "get";
+    }
+
 }
