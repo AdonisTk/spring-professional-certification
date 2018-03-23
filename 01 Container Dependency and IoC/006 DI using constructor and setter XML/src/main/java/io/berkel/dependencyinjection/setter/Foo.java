6c867c26becc98f3ea2bb2bbcd2c5b7c6eef0f13
Fabio Gaiera <fagaiera@users.noreply.github.com>
2018-03-23 18:00:05 +0200

    @Autowired

--- 01 Container Dependency and IoC/006 DI using constructor and setter XML/src/main/java/io/berkel/dependencyinjection/setter/Foo.java
+++ 01 Container Dependency and IoC/006 DI using constructor and setter XML/src/main/java/io/berkel/dependencyinjection/setter/Foo.java
@@ -1,10 +1,13 @@
 package io.berkel.dependencyinjection.setter;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class Foo {
 
     private Bar1 bar1;
     private Bar2 bar2;
 
+    @Autowired
     public void setBar1(Bar1 bar1) {
         this.bar1 = bar1;
     }
@@ -13,6 +16,7 @@ public class Foo {
         return bar2;
     }
 
+    @Autowired
     public void setBar2(Bar2 bar2) {
         this.bar2 = bar2;
     }
