3209f286f0f6e34e462d99a6d26f35b8cd71841f
Fabio Gaiera <fagaiera@users.noreply.github.com>
2018-05-04 14:01:10 +0300

    Private field.

--- 01 Container Dependency and IoC/028 ReflectionTestUtils/src/main/java/io/berkel/reflectiontestutils/model/Person.java
+++ 01 Container Dependency and IoC/028 ReflectionTestUtils/src/main/java/io/berkel/reflectiontestutils/model/Person.java
@@ -12,14 +12,19 @@ public class Person {
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public String getSurname() {
 		return surname;
 	}
 	public void setSurname(String surname) {
 		this.surname = surname;
 	}
+
 	public int getId() {
 		return id;
 	}
-	
+	private void setId(int id) {
+        this.id = id;
+    }
+
 }
