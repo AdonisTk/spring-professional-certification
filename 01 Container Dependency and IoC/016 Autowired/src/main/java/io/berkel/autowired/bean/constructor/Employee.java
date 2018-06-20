83481b1c69b6ac27c32585ebd2baba0de76400eb
Berkelio Software <fagaiera@hotmail.it>
2018-06-20 20:11:47 +0100

    @Autowired.

--- 01 Container Dependency and IoC/016 Autowired/src/main/java/io/berkel/autowired/bean/constructor/Employee.java
+++ 01 Container Dependency and IoC/016 Autowired/src/main/java/io/berkel/autowired/bean/constructor/Employee.java
@@ -1,10 +1,15 @@
 package io.berkel.autowired.bean.constructor;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 
 public class Employee {
 
     private Company company;
+
+    @Value("#{'Fabio Gaiera'}")
+    private String name;
+
 
     @Autowired
     public Employee(Company company) {
@@ -17,6 +22,14 @@ public class Employee {
 
     public void setCompany(Company company) {
         this.company = company;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 
 }
